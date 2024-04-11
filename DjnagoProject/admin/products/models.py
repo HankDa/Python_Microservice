eca60c73c1d2f36567fb7d5371bf1fd3139f5c7a
HankDa <muhd0416@gmail.com>
2024-04-11 17:25:19 +0100

    - Django: admin-> products 	- create user,product model 	- create viewset 		-- completed list function 	- create product.urls.py

--- DjnagoProject/admin/products/models.py
+++ DjnagoProject/admin/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+'''
+- generate migration file: python manage.py makemigrations
+- apply migration to database: python manage.py migrate
+'''
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    like = models.PositiveIntegerField(default=0)
+
+
+class User(models.Model):
+    '''
+    only have id column
+    '''
+    pass
--- DjnagoProject/admin/products/views.py
+++ DjnagoProject/admin/products/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): 
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+        
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
+
